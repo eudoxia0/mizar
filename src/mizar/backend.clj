2b3b43e7cdc82737bf58ffd2d4e24abe073bd9e4
Fernando Borretti <eudoxiahp@gmail.com>
2016-06-21 19:33:31 -0300

    Fix the backend

--- src/mizar/backend.clj
+++ src/mizar/backend.clj
@@ -1,5 +1,6 @@
 (ns mizar.backend
-  "Turn the AST to C.")
+  "Turn the AST to C."
+  (:require [clojure.string :refer [join]]))
 
 (defn emit
   "Turn an AST node into a C string."
@@ -7,10 +8,11 @@
   (case (:type node)
     :program (join (emit (:children node)))
 
-    :defn (join (fmt "%s %s(%s) {"
-                     (emit (:return-type node))
-                     (emit (:name node))
-                     (join ", " (map (fn [arg]
-                                       (fmt "%s %s" (:type arg) (:name arg)))
-                                     (:args node))))
-                "}")))
+    :defn (join (format "%s %s(%s) {"
+                        (emit (:return-type node))
+                        (emit (:name node))
+                        (join ", " (map (fn [arg]
+                                          (format "%s %s" (:type arg) (:name arg)))
+                                        (:args node))))
+                "}")
+    ""))
