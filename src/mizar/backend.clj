0337061695b8e88c70655d0d1a0b9fdfdf02bed8
Fernando Borretti <eudoxiahp@gmail.com>
2016-06-21 19:47:14 -0300

    Implement some of the backend

--- src/mizar/backend.clj
+++ src/mizar/backend.clj
@@ -2,17 +2,44 @@
   "Turn the AST to C."
   (:require [clojure.string :refer [join]]))
 
+(def runtime
+  "#include <stdio.h>
+
+#define true 1
+#define false 0
+
+typedef unsigned char bool;")
+
 (defn emit
   "Turn an AST node into a C string."
   [node]
   (case (:type node)
-    :program (join (emit (:children node)))
+    :program (format "%s\n\n%s" runtime (join (map emit (:children node))))
 
-    :defn (join (format "%s %s(%s) {"
-                        (emit (:return-type node))
-                        (emit (:name node))
-                        (join ", " (map (fn [arg]
-                                          (format "%s %s" (:type arg) (:name arg)))
-                                        (:args node))))
-                "}")
+    :defn (format "%s %s(%s) {\n%s\n}"
+                  (emit (:return-type node))
+                  (emit (:name node))
+                  (join ", " (map (fn [arg]
+                                    (format "%s %s"
+                                            (emit (:type arg))
+                                            (emit (:name arg))))
+                                  (:args node)))
+                  (emit (:body node)))
+
+    :if (format "if (%s) {\n%s\n} else {\n%s\n}"
+                (emit (:test node))
+                (emit (:consequent node))
+                (emit (:alternate node)))
+    :block (join ";\n" (map emit (:children node)))
+    :return (format "return %s;" (emit (:value node)))
+
+    :binop (format "%s %s %s"
+                   (emit (:lhs node))
+                   (emit (:op node))
+                   (emit (:rhs node)))
+
+    :constant (:value node)
+    :type (emit (:def node))
+    :ident (:value node)
+
     ""))
