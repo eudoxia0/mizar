9003608a634fdfb108773d6efc6012496485a1da
Fernando Borretti <eudoxiahp@gmail.com>
2016-06-21 18:54:25 -0300

    More parser tests

--- test/mizar/parser_test.clj
+++ test/mizar/parser_test.clj
@@ -2,8 +2,23 @@
   (:require [clojure.test :refer :all]
             [mizar.parser :refer :all]))
 
+(defmacro is-constant [code type value]
+  (let [cst (gensym)]
+    `(let [~cst (grammar ~code :start :constant)]
+       (println ~cst)
+       (is (= (first ~cst)
+              :constant))
+       (is (= (first (second ~cst))
+              ~type))
+       (is (= (second (second ~cst))
+              ~value)))))
+
 (deftest parsing
-  (testing "Simple Function"
+  (testing "Constants"
+    (is-constant "true" :bool "true")
+    (is-constant "false" :bool "false")
+    (is-constant "123" :integer "123"))
+  (testing "Toplevel"
     (let [code "int add(a:int b:int) begin
   return a+b
 end"]
