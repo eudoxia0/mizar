cf6957a00e4e4744fa84b02f5551a26602428d0c
Fernando Borretti <eudoxiahp@gmail.com>
2016-06-21 19:33:35 -0300

    Fix backend tests

--- test/mizar/backend_test.clj
+++ test/mizar/backend_test.clj
@@ -1,5 +1,16 @@
-(ns mizar.ast-test
+(ns mizar.backend-test
   (:require [clojure.test :refer :all]
             [mizar.parser :refer [grammar]]
             [mizar.ast :refer [transform]]
             [mizar.backend :refer :all]))
+
+(deftest ast-to-c
+  (testing "Simplest Function"
+    (let [code "bool pass(b:bool) begin
+  if b then
+    return true
+  else
+    return false
+end"
+          c (emit (transform (grammar code)))]
+      (is (string? c)))))
